6c2d68d6bdf7d1ab4a015acd986cad3d4035f07f
omar soto <sotoanaya@gmail.com>
2019-09-25 00:23:53 -0500

    First load

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vlidly.Models;
+
+namespace Vlidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Omar Soto"},
+                new Customer() {Name = "Magda Soto"}
+            };
+
+            return View(customers);
+        }
+    }
+}
